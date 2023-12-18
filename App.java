a24896f606509cdcf14df1367e0a41654f8aaa05
PotatoMozzarella <148605894+TheSProyect@users.noreply.github.com>
2023-12-17 23:54:57 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,152 @@
+import java.util.Scanner;
+import java.io.*;
+import javax.swing.*; 
+import javax.swing.event.*;
+import java.awt.event.*; 
+import java.awt.*; 
+
+public class App extends JFrame implements ActionListener{
+  private JLabel insertData, descLabel, bsLabel, ciLabel, nfLabel, dateLabel, amountLabel;
+  private JButton closeButton, registerButton, reportButton;
+  private JTextField descField, amountField, bsField, ciField, dateField, nfField;
+
+  
+
+
+
+  public App(){
+    setTitle("Registro y Control de Equipos en Centro de Investigación");
+
+    setLayout(null);
+    insertData = new JLabel("Ingrese data del equipo:"); 
+    insertData.setBounds(10, 20, 200, 20);
+    add(insertData);
+
+    descLabel = new JLabel("Descripción:");
+    descLabel.setBounds(15, 60, 200, 20);
+    add(descLabel);
+
+    descField = new JTextField();
+    descField.setBounds(90, 60, 200, 20);
+    add(descField);
+
+    amountLabel = new JLabel("Cantidad:");
+    amountLabel.setBounds(25, 100, 200, 20);
+    add(amountLabel);
+
+    amountField = new JTextField();
+    amountField.setBounds(90, 100, 70, 20);
+    add(amountField);
+    
+    bsLabel = new JLabel("Costo Unitario (Bs.):");
+    bsLabel.setBounds(310, 100, 200, 20);
+    add(bsLabel);
+
+    bsField = new JTextField();
+    bsField.setBounds(430, 100, 110, 20);
+    add(bsField);
+
+    dateLabel = new JLabel("<html>Fecha de adquisicion:<br>dd/mm/aaaa</html>");
+    dateLabel.setBounds(15, 140, 200, 30);
+    add(dateLabel);
+
+    dateField = new JTextField();
+    dateField.setBounds(140, 140, 120, 20);
+    add(dateField);
+
+    nfLabel = new JLabel("Nro. de Factura:");
+    nfLabel.setBounds(330, 140, 200, 20);
+    add(nfLabel);
+
+    nfField = new JTextField();
+    nfField.setBounds(430, 140, 110, 20);
+    add(nfField);
+
+    ciLabel = new JLabel("C.I. del Responsable del equipo:");
+    ciLabel.setBounds(15, 180, 200, 20);
+    add(ciLabel);
+
+    ciField = new JTextField();
+    ciField.setBounds(200, 180, 200, 20);
+    add(ciField);
+
+    registerButton = new JButton("Registrar data");
+    registerButton.setBounds(170, 260, 140, 30);
+    add(registerButton);
+    registerButton.addActionListener(this);
+
+    reportButton = new JButton("Generar Reporte");
+    reportButton.setBounds(310, 260, 140, 30);
+    add(reportButton);
+    reportButton.addActionListener(this);
+
+    closeButton = new JButton("Salir");
+    closeButton.setBounds(450, 260, 100, 30);
+    add(closeButton);
+    closeButton.addActionListener(this);
+
+
+  }
+
+  public void actionPerformed(ActionEvent Event){
+    if(Event.getSource() == closeButton){
+      System.exit(0);
+    }
+
+    if(Event.getSource() == registerButton){
+      String desc = descField.getText().trim();
+      String ci = ciField.getText().trim();
+      String date = dateField.getText().trim();
+      String nf = nfField.getText().trim();
+      String ct = amountField.getText().trim();
+      String mu = bsField.getText().trim();
+
+      if (desc.equals("") || ci.equals("") || date.equals("") || nf.equals("") || ct.equals("") || mu.equals("")){
+        JOptionPane.showMessageDialog(null, "Se deben llenar todos los campos con datos validos");
+      } else {
+      try {
+      BufferedWriter writer = new BufferedWriter(new FileWriter("inventario.txt"));
+      writer.write("\n" + desc + "#" + ct + "#" + mu + "#" + date + "#" + nf + "#" + ci + ";");
+      writer.close();
+      
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+
+    } 
+    if(Event.getSource() == reportButton){
+      Report IReporte = new Report();
+      IReporte.setBounds(0,0, 600, 350);
+      IReporte.setVisible(true);
+      IReporte.setLocationRelativeTo(null);
+      IReporte.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      IReporte.setResizable(false);
+      IReporte.setTitle("Reporte del Inventario del Centro de Investigación");
+      this.setVisible(false);
+      
+      
+    }
+  }
+
+  
+
+
+  public static void main(String[] args) throws Exception {
+    
+    App ICentro = new App();
+
+    ICentro.setBounds(0,0, 600, 350);
+    ICentro.setVisible(true);
+    ICentro.setLocationRelativeTo(null);
+    ICentro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    ICentro.setResizable(false);
+    
+
+    
+    
+  }
+
+
+}
---
+++ Report.java
@@ -0,0 +1,155 @@
+import java.util.Scanner;
+import java.io.*;
+import javax.swing.*; 
+import javax.swing.event.*;
+import java.awt.event.*; 
+import java.awt.*; 
+
+public class Report extends JFrame implements ActionListener{
+    private JLabel reportType, ciLabel, total, quantity, money;
+    private JTextField ciField;
+    private JTextArea generalTable;
+    private JScrollPane Scroller;
+    private JButton continueButton, totalizeButton;
+    private JRadioButton individualButton, generalButton;
+    private ButtonGroup bg;
+    private JPanel individualReport, generalReport;
+
+    public Report(){
+        setLayout(null);
+
+        int amount = 0;
+        int bs = 0;
+
+        reportType = new JLabel("Tipo de Reporte:");
+        reportType.setBounds(10, 20, 200, 20);
+        add(reportType);
+
+        individualButton = new JRadioButton("Individual");
+        individualButton.setBounds(100, 20, 100, 20);
+        individualButton.addActionListener(this);
+        add(individualButton);
+
+        generalButton = new JRadioButton("General");
+        generalButton.setBounds(200, 20, 200, 20);
+        generalButton.addActionListener(this);
+        add(generalButton);
+        
+        bg = new ButtonGroup();
+        bg.add(individualButton);
+
+        bg.add(generalButton);
+
+        continueButton = new JButton("Continuar");
+        continueButton.setBounds(450, 260, 100, 30);
+        add(continueButton);
+        continueButton.addActionListener(this);
+
+        ciLabel = new JLabel("C.I. del Responsable de Equipos:");
+        ciLabel.setBounds(10, 30, 300, 20);
+        ciField = new JTextField();
+        ciField.setBounds(200, 30, 200, 20);
+
+        totalizeButton = new JButton("Totalizar");
+        totalizeButton.setBounds(410, 30, 100, 20);
+        totalizeButton.addActionListener(this);
+
+        generalTable = new JTextArea();
+        Scroller = new JScrollPane(generalTable);
+        Scroller.setBounds(30,10, 520, 120);
+
+        individualReport = new JPanel();
+        individualReport.setLayout(null);
+        individualReport.setBounds(0, 50, 600, 190);
+        individualReport.add(ciLabel);
+        individualReport.add(ciField);
+        individualReport.add(totalizeButton);
+
+        add(individualReport);
+            
+        generalReport = new JPanel();
+        generalReport.setLayout(null);
+        generalReport.setBounds(0, 50, 600, 190);
+        generalReport.add(Scroller);
+
+
+        add(generalReport);
+
+        total = new JLabel("Totalizacion:");
+        total.setBounds(10, 240, 100, 20);
+        add(total);
+
+        money = new JLabel(bs + " Bs.");
+        money.setBounds(10, 255, 100, 20);
+        add(money);
+
+        quantity = new JLabel(amount + " equipos");
+        quantity.setBounds(10, 270, 100, 20);
+        add(quantity);
+
+    
+    }
+
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == individualButton){
+            individualReport.setVisible(true);
+            generalReport.setVisible(false);
+            
+        }
+        
+        if(e.getSource() == generalButton){
+            individualReport.setVisible(false);
+            generalReport.setVisible(true);
+        }
+        if(e.getSource() == continueButton){
+            App ICentro = new App();
+
+        ICentro.setBounds(0,0, 600, 350);
+        ICentro.setVisible(true);
+        ICentro.setLocationRelativeTo(null);
+        ICentro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ICentro.setResizable(false);
+        this.setVisible(false);
+            
+        }
+        if(e.getSource() == totalizeButton){
+            String ci = ciField.getText().trim();
+            if(ci.equals("")){
+                JOptionPane.showMessageDialog(null, "Ingrese una Cedula");
+
+            }else{
+                try{
+                BufferedReader reader = new BufferedReader(new FileReader("inventario.txt"));
+                String Line;
+                while ((Line = reader.readLine()) != null){
+
+                String Format = Line;
+                String[] parts = Format.split("#");
+                //this.amount = amount + Integer.parseInt(parts[1]);
+                //this.bs = bs + Float.parseFloat(parts[2]);
+                
+            }
+                reader.close();
+                } catch (IOException ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+        
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        Report IReporte = new Report();
+        IReporte.setBounds(0,0, 600, 350);
+        IReporte.setVisible(true);
+        IReporte.setLocationRelativeTo(null);
+        IReporte.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        IReporte.setResizable(false);
+        IReporte.setTitle("Reporte del Inventario del Centro de Investigación");
+
+
+
+
+    }
+}
